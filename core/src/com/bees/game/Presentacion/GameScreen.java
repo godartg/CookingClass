55a6d25edf370d2c19cb1cee83338810cc67c5ca
Edwin <sl.vilcapuma.edwin@gmail.com>
2018-04-08 18:30:16 -0500

    Creación del objeto enum ScreenEnum
    Permitirá gestionar las escenas

--- core/src/com/bees/game/Presentacion/GameScreen.java
+++ core/src/com/bees/game/Presentacion/GameScreen.java
@@ -2,7 +2,6 @@ package com.bees.game.Presentacion;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
@@ -16,7 +15,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.bees.game.Entidad.EIngrediente;
 import com.bees.game.Entidad.ELugarPrep;
---
+++ core/src/com/bees/game/utils/ScreenEnum.java
@@ -0,0 +1,57 @@
+package com.bees.game.utils;
+
+import com.bees.game.Entidad.Evaluacion;
+import com.bees.game.Presentacion.BaseScreen;
+import com.bees.game.Presentacion.GameScreen;
+import com.bees.game.Presentacion.LoadingScreen;
+import com.bees.game.Presentacion.MenuScreen;
+import com.bees.game.Presentacion.ScoreScreen;
+
+/**
+ * Para gestionar las escenas, crearemos un tipo Enum de java, llamado ScreenEnum, con la lista de
+ * pantallas disponibles. Además, el enumerado tiene un método getScreen() que utilizaremos para
+ * obtener cada pantalla a partir de su tipo enumerado.
+ *
+ * Created by workaholic on 8/04/2018.
+ * @see <a href="http://www.pixnbgames.com/blog/libgdx/how-to-manage-screens-in-libgdx/">How to manage screens in LibGDX</a>
+ */
+
+public enum ScreenEnum {
+    LOADING_SCREEN{
+        public BaseScreen getScreen(Object... params){
+            return new LoadingScreen();
+        }
+    },
+    MENU_SCREEN{
+        public BaseScreen getScreen(Object... params){
+            return new MenuScreen();
+        }
+    },
+    CONFIGURACION_SCREEN{
+        public BaseScreen getScreen(Object... params){
+            return new ConfiguracionScreen();
+        }
+    },
+    TUTORIAL_SCREEN{
+        public BaseScreen getScreen(Object... params){
+            return new TutorilScreen();
+        }
+    },
+    SELECCION_LEVEL_SCREEN{
+        public BaseScreen getScreen(Object... params){
+            return new SelectionLevelScreen();
+        }
+    },
+    GAME_SCREEN{
+        public BaseScreen getScreen(Object... params){
+            return new GameScreen((Integer) params[0]);
+        }
+    },
+    SCORE_SCREEN{
+        public BaseScreen getScreen(Object... params){
+            return new ScoreScreen((Evaluacion) params[0]);
+        }
+    }
+
+    public abstract BaseScreen getScreen(Object... params);
+}
