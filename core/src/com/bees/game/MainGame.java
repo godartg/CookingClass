a2a5566f2f15ca4578bbce6ca1b66b1af44e9e98
Edwin <sl.vilcapuma.edwin@gmail.com>
2018-04-10 00:01:09 -0500

    Implementación de AnnotationAssetManager en clase MainGame

--- core/src/com/bees/game/MainGame.java
+++ core/src/com/bees/game/MainGame.java
@@ -5,84 +5,30 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.bees.game.Presentacion.SelectionLevelScreen;
+import com.bees.game.utils.AssetsManager;
+import com.bees.game.utils.ScreenEnum;
+import com.bees.game.utils.ScreenManager;
+
+import net.dermetfan.gdx.assets.AnnotationAssetManager;
 
 /**
- * Esta es la clase principal la cual heredará de la clase Game la cual nos permite crear multiples pantallas
+ * Esta es la clase principal la cual nos permite inicializar las pantallas
+ * Tambien cargamos los recursos con la clase AnnotationAssetManager
  * @author Edwin César Condori Vilcapuma
  * @author Mauricio García Silva
  * Creado por Edwin César Condori Vilcapuma y Mauricio García Silva  20/03/2018.
- * @see  <a href= "https://www.youtube.com/watch?v=csNXZgka5-o"/> LIBGDX para Android - Tutorial 14 - Multiples Pantallas </a>
+ * @see <a href= "http://www.pixnbgames.com/blog/libgdx/como-gestionar-varias-pantallas-en-libgdx/"/>Cómo gestionar varias pantallas en LibGDX</a>
+ * @see <a href="https://bitbucket.org/dermetfan/libgdx-utils/wiki/net.dermetfan.gdx.assets.AnnotationAssetManager">AnnotationAssetManager</a>
  */
 
 public class MainGame extends Game{
-
-    private AssetManager manager;
-    private Music backgroundMusic;
-
-    public com.bees.game.Presentacion.BaseScreen loadingScreen, tituloScreen, menuScreen, gameScreen, instructionScreen, scoreScreen, selectioningredienteScreen;
-
+    public static final AnnotationAssetManager manager = new AnnotationAssetManager();
     @Override
     public void create () {
-        manager = new AssetManager();
-
-        //Se agrego los ultimos recursos
-        //el nombre de la carpeta fue cambiado porque no puede haber espacio entre los caracteres
-        //pantallas
-        manager.load("recursos_imagenes/intro.jpg", Texture.class);
-        manager.load("recursos_imagenes/supermarket.png", Texture.class);
-        manager.load("recursos_imagenes/salon_clases.jpg", Texture.class);
-        manager.load("recursos_imagenes/puntuacion.png", Texture.class);
-
-        manager.load("recursos_imagenes/logo.jpg", Texture.class);
-        manager.load("recursos_imagenes/logob.jpg", Texture.class);
-        manager.load("recursos_imagenes/tutorial1.png", Texture.class);
-        manager.load("recursos_imagenes/tutorial2.png", Texture.class);
-        manager.load("recursos_imagenes/tutorial3.jpg", Texture.class);
-        manager.load("recursos_imagenes/tutorial4.jpg", Texture.class);
-        manager.load("recursos_imagenes/tutorial5.png", Texture.class);
-        manager.load("recursos_imagenes/tutorial6.jpg", Texture.class);
-        manager.load("recursos_imagenes/comidas1a.jpg", Texture.class);
-
-        manager.load("recursos_imagenes/butter.png", Texture.class);
-        manager.load("recursos_imagenes/milk.png", Texture.class);
-        manager.load("recursos_imagenes/potato.png", Texture.class);
-        manager.load("recursos_imagenes/sal.png", Texture.class);
-
-        manager.load("recursos_imagenes/olla.png", Texture.class);
-        manager.load("recursos_imagenes/posillo.png", Texture.class);
-        manager.load("recursos_imagenes/sarten.png", Texture.class);
-        manager.load("recursos_imagenes/tabla.png", Texture.class);
-
-        manager.load("audio/jump.ogg", Sound.class);
-        manager.load("audio/Menuet.ogg", Music.class);
-        manager.load("audio/Wiener.ogg", Music.class);
-        manager.load("recursos_sonidos/Bolero.mp3", Music.class);
-
-        loadingScreen = new com.bees.game.Presentacion.LoadingScreen(this);
-        setScreen(loadingScreen);
+        manager.load(AssetsManager.class);
+        ScreenManager.getInstance().initialize(this);
+        ScreenManager.getInstance().showScreen( ScreenEnum.LOADING_SCREEN );
     }
 
-    public void finishLoading() {
-        backgroundMusic = getManager().get("audio/Menuet.ogg");
-        backgroundMusic.setVolume(1);
-        backgroundMusic.play();
-        tituloScreen = new com.bees.game.Presentacion.TituloScreen(this);
-        menuScreen = new com.bees.game.Presentacion.MenuScreen(this);
-        instructionScreen = new com.bees.game.Presentacion.InstructionScreen(this);
-        selectioningredienteScreen= new com.bees.game.Presentacion.SelectionIngredienteScreen(this);
-        setScreen(tituloScreen);
-    }
-    public void startGame(String ingredientes){
-        backgroundMusic.stop();
-        gameScreen = new com.bees.game.Presentacion.GameScreen(this, ingredientes);
-        setScreen(gameScreen);
-    }
-    public void evaluar(int score){
-        scoreScreen = new com.bees.game.Presentacion.ScoreScreen(this, score);
-        setScreen(scoreScreen);
-    }
-
-    public AssetManager getManager() {
-        return manager;
-    }
 }
--- core/src/com/bees/game/utils/ScreenManager.java
+++ core/src/com/bees/game/utils/ScreenManager.java
@@ -6,6 +6,7 @@ import com.bees.game.Presentacion.BaseScreen;
 
 /**Esta clase, en combinación con ScreenEnum, nos permitirá cambiar de pantalla facilmente.
  * En esta clase se usa el patrón Singleton.
+ * @see <a href="http://www.pixnbgames.com/blog/libgdx/como-gestionar-varias-pantallas-en-libgdx/">Cómo gestionar varias pantallas en LibGDX</a>
  * Created by workaholic on 8/04/2018.
  */
 
